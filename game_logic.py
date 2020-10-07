18e57c25b90c9f141f9bf74809ec89adb230dc1d
Prabhu Marappan <prabhum.794@gmail.com>
2020-10-08 01:41:01 +0530

    Create game_logic file with just the lofic for the game

---
+++ game_logic.py
@@ -0,0 +1,34 @@
+import random
+import string
+from select import select
+
+def start_game(conn, address):
+    score = 0
+    while True:
+        random_string = random.choice(string.ascii_letters)
+        conn.setblocking(0)
+        data = random_string + "\n"
+        conn.send(data.encode())  # send data to the client
+        # # receive data stream. it won't accept data packet greater than 1024 bytes
+        timeout = 5
+        rlist, wlist, xlist = select([conn], [], [], timeout)
+        if rlist:
+            recieved_data = conn.recv(1024).decode().strip()
+            if random_string == recieved_data:
+                score += 1
+                result = "Matched the sent! score is now %s\n" % score
+                conn.send(result.encode())
+            else:
+                score -= 1
+                result = "does not match the sent! score is now %s\n" % score
+                conn.send(result.encode())
+        else:
+            result = "Timed out!\n"
+            conn.send(result.encode())
+
+        if score == -3 or score == 10:
+            data = "Game Over! Your score is %s\n" % score
+            conn.send(data.encode())
+            break
+
+    conn.close()
