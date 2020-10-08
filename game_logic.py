76f5efbb45a338bc6328ce1ba363c8c193a6a0a8
Prabhu Marappan <prabhum.794@gmail.com>
2020-10-08 22:52:49 +0530

    Implement timeout retries and ending game at 3 timeouts

--- game_logic.py
+++ game_logic.py
@@ -22,9 +22,16 @@ def start_game(conn, address):
                 score -= 1
                 result = "does not match the sent! score is now %s\n" % score
                 conn.send(result.encode())
+            timeout_retries = 0
         else:
             result = "Timed out!\n"
+            timeout_retries += 1
             conn.send(result.encode())
+
+        if timeout_retries == 3:
+            data = "Three continious retires over, game is now exiting\n Your score is %s" % score
+            conn.send(data.encode())
+            break
 
         if score == -3 or score == 10:
             data = "Game Over! Your score is %s\n" % score
