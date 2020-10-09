a51b499def33003c70e0f45897bdd57a9b2f885a
Prabhu Marappan <prabhum.794@gmail.com>
2020-10-09 22:17:26 +0530

    Added comments to function in server

--- server.py
+++ server.py
@@ -4,23 +4,28 @@ from game_logic import start_game
 import os
 
 def get_port_number():
+    """
+    Function to get the port number on which to run the server or use the default one
+    """
     port_number = os.getenv("SOCKET_GAME_PORT", 6018)
     port_number = int(port_number)
     return port_number
 
 def server_program():
-    # get the hostname
+    """
+    Function to start the server and listen for new connection and start a new thread for each 
+    connection
+    """
     host = socket.gethostname()
     port = get_port_number()
 
-    server_socket = socket.socket()  # get instance
+    server_socket = socket.socket() 
 
-    server_socket.bind((host, port))  # bind host address and port together
+    server_socket.bind((host, port))
 
-    # configure how many client the server can listen simultaneously
     server_socket.listen(1000)
     while True:
-        conn, address = server_socket.accept()  # accept new connection
+        conn, address = server_socket.accept()
         print("Connection from: " + str(address))
         threading.Thread(target=start_game, args=[conn, address]).start()
 
